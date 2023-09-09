76d46d17e05966ca1ca506725a45c21c3fd64ffd
kpachauri2 <krishpachauri4@gmail.com>
2023-09-09 15:08:13 +0530

    login 3files

--- js/login.js
+++ js/login.js
@@ -13,7 +13,21 @@
 
 function onSignIn(googleUser) {
     // Handle Google Sign-In (use the onSignIn function from previous code)
+    var profile = googleUser.getBasicProfile();
+    $("#name").text(profile.getName());
+    $("#email").text(profile.getEmail());
+    $("#image").attr("src",profile.getImage());
+    $(".data").css("display","block");
+    $(".g-signin2").css("display","none");
 }
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      alert("you have been signed our successfully");
+      $(".g-signin2").css("display","block");
+      $(".data").css("display","none");
+    });
+  }
 
 // Add JavaScript code to handle form submission (e.g., sending login data to the server)
 document.getElementById("login-form").addEventListener("submit", function (event) {
